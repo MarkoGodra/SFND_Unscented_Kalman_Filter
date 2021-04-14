0b027cc140f060c634fc287add17574561b2b158
MarkoGodra <marko.godra@gmail.com>
2021-04-14 16:00:18 +0200

    Reformat

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -139,15 +139,15 @@ void UKF::Prediction(double delta_t)
     // create sigma point matrix
     MatrixXd Xsig_aug = MatrixXd(n_aug_, 2 * n_aug_ + 1);
     Xsig_aug.col(0) = x_aug;
-    double multiplier = sqrt(lambda_ + (double)n_aug_);
-    for(int i = 0; i < n_aug_; i++)
+    double multiplier = sqrt(lambda_ + (double) n_aug_);
+    for (int i = 0; i < n_aug_; i++)
     {
         Xsig_aug.col(i + 1) = x_aug + multiplier * A.col(i);
         Xsig_aug.col(n_aug_ + 1 + i) = x_aug - multiplier * A.col(i);
     }
 
     // 2) Predict sigma points
-    for(int i = 0; i < 2 * n_aug_ + 1; i++)
+    for (int i = 0; i < 2 * n_aug_ + 1; i++)
     {
         double px = Xsig_aug(0, i);
         double py = Xsig_aug(1, i);
@@ -161,10 +161,10 @@ void UKF::Prediction(double delta_t)
         double ppx, ppy, pv, pyaw, pyawd;
 
         // Pass through CTRV model
-        if(fabs(yaw) > 0.001)
+        if (fabs(yaw) > 0.001)
         {
-            ppx = px + v/yawd * (sin(yaw + yawd * delta_t) - sin(yaw));
-            ppy = py + v/yawd * (cos(yaw) - cos(yaw + yawd * delta_t));
+            ppx = px + v / yawd * (sin(yaw + yawd * delta_t) - sin(yaw));
+            ppy = py + v / yawd * (cos(yaw) - cos(yaw + yawd * delta_t));
         }
         else
         {
@@ -199,22 +199,24 @@ void UKF::Prediction(double delta_t)
 
     // predict state mean
     x.fill(0.0);
-    for(int  i = 0; i < 2 * n_aug_ + 1; i++)
+    for (int i = 0; i < 2 * n_aug_ + 1; i++)
     {
         x = x + weights_(i) * Xsig_pred_.col(i);
     }
 
     // predict state covariance matrix
     P.fill(0.0);
-    for(int  i = 0; i < 2 * n_aug_ + 1; i++)
+    for (int i = 0; i < 2 * n_aug_ + 1; i++)
     {
         // state difference
         VectorXd x_diff = Xsig_pred_.col(i) - x;
         // angle normalization
-        while (x_diff(3)> M_PI) x_diff(3)-=2.*M_PI;
-        while (x_diff(3)<-M_PI) x_diff(3)+=2.*M_PI;
+        while (x_diff(3) > M_PI)
+        { x_diff(3) -= 2. * M_PI; }
+        while (x_diff(3) < -M_PI)
+        { x_diff(3) += 2. * M_PI; }
 
-        P = P + weights_(i) * x_diff * x_diff.transpose() ;
+        P = P + weights_(i) * x_diff * x_diff.transpose();
     }
     std::cout << "x_: " << std::endl << x << std::endl;
     std::cout << "P_: " << std::endl << P << std::endl;
