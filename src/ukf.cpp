e1e40d9e21e72b59c9401a075f4db8776b8f0df6
MarkoGodra <marko.godra@gmail.com>
2021-04-14 13:59:04 +0200

    Reformat proj

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -7,61 +7,62 @@ using Eigen::VectorXd;
 /**
  * Initializes Unscented Kalman filter
  */
-UKF::UKF() {
-  // if this is false, laser measurements will be ignored (except during init)
-  use_laser_ = true;
+UKF::UKF()
+{
+    // if this is false, laser measurements will be ignored (except during init)
+    use_laser_ = true;
 
-  // if this is false, radar measurements will be ignored (except during init)
-  use_radar_ = true;
+    // if this is false, radar measurements will be ignored (except during init)
+    use_radar_ = true;
 
-  n_x_ = 5;
+    n_x_ = 5;
 
-  // initial state vector
-  x_ = VectorXd(n_x_);
-  x_ << 0.0, 0.0, 0.0, 0.0, 0.0;
+    // initial state vector
+    x_ = VectorXd(n_x_);
+    x_ << 0.0, 0.0, 0.0, 0.0, 0.0;
 
-  // initial covariance matrix
-  P_ = MatrixXd(n_x_, n_x_);
-  P_ << 1.0, 0.0, 0.0, 0.0, 0.0,
-          0.0, 1.0, 0.0, 0.0, 0.0,
-          0.0, 0.0, 1.0, 0.0, 0.0,
-          0.0, 0.0, 0.0, 1.0, 0.0,
-          0.0, 0.0, 0.0, 0.0, 1.0;
+    // initial covariance matrix
+    P_ = MatrixXd(n_x_, n_x_);
+    P_ << 1.0, 0.0, 0.0, 0.0, 0.0,
+            0.0, 1.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 1.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 1.0, 0.0,
+            0.0, 0.0, 0.0, 0.0, 1.0;
 
-  // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 30;
+    // Process noise standard deviation longitudinal acceleration in m/s^2
+    std_a_ = 30;
 
-  // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 30;
-  
-  /**
-   * DO NOT MODIFY measurement noise values below.
-   * These are provided by the sensor manufacturer.
-   */
+    // Process noise standard deviation yaw acceleration in rad/s^2
+    std_yawdd_ = 30;
 
-  // Laser measurement noise standard deviation position1 in m
-  std_laspx_ = 0.15;
+    /**
+     * DO NOT MODIFY measurement noise values below.
+     * These are provided by the sensor manufacturer.
+     */
 
-  // Laser measurement noise standard deviation position2 in m
-  std_laspy_ = 0.15;
+    // Laser measurement noise standard deviation position1 in m
+    std_laspx_ = 0.15;
 
-  // Radar measurement noise standard deviation radius in m
-  std_radr_ = 0.3;
+    // Laser measurement noise standard deviation position2 in m
+    std_laspy_ = 0.15;
 
-  // Radar measurement noise standard deviation angle in rad
-  std_radphi_ = 0.03;
+    // Radar measurement noise standard deviation radius in m
+    std_radr_ = 0.3;
 
-  // Radar measurement noise standard deviation radius change in m/s
-  std_radrd_ = 0.3;
-  
-  /**
-   * End DO NOT MODIFY section for measurement noise values 
-   */
-  
-  /**
-   * TODO: Complete the initialization. See ukf.h for other member properties.
-   * Hint: one or more values initialized above might be wildly off...
-   */
+    // Radar measurement noise standard deviation angle in rad
+    std_radphi_ = 0.03;
+
+    // Radar measurement noise standard deviation radius change in m/s
+    std_radrd_ = 0.3;
+
+    /**
+     * End DO NOT MODIFY section for measurement noise values
+     */
+
+    /**
+     * TODO: Complete the initialization. See ukf.h for other member properties.
+     * Hint: one or more values initialized above might be wildly off...
+     */
 
     time_us_ = 0.0;
 
@@ -71,61 +72,66 @@ UKF::UKF() {
 
     weights_ = VectorXd(2 * n_aug_ + 1);
     weights_(0) = lambda_ / (lambda_ + n_aug_);
-    for(int  i = 0; i < 2 * n_aug_; i++)
+    for (int i = 0; i < 2 * n_aug_; i++)
     {
         double weight = 0.5 / (lambda_ + n_aug_);
         weights_(i + 1) = weight;
     }
 }
 
-UKF::~UKF() {}
+UKF::~UKF()
+{}
 
-void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
-  /**
-   * TODO: Complete this function! Make sure you switch between lidar and radar
-   * measurements.
-   */
+void UKF::ProcessMeasurement(MeasurementPackage meas_package)
+{
+    /**
+     * TODO: Complete this function! Make sure you switch between lidar and radar
+     * measurements.
+     */
 
-  if(!is_initialized_)
-  {
-      InitUKF(meas_package);
-  }
-  else
-  {
-      // Regular flow from assignments
-  }
+    if (!is_initialized_)
+    {
+        InitUKF(meas_package);
+    }
+    else
+    {
+        // Regular flow from assignments
+    }
 }
 
-void UKF::Prediction(double delta_t) {
-  /**
-   * TODO: Complete this function! Estimate the object's location. 
-   * Modify the state vector, x_. Predict sigma points, the state, 
-   * and the state covariance matrix.
-   */
+void UKF::Prediction(double delta_t)
+{
+    /**
+     * TODO: Complete this function! Estimate the object's location.
+     * Modify the state vector, x_. Predict sigma points, the state,
+     * and the state covariance matrix.
+     */
 }
 
-void UKF::UpdateLidar(MeasurementPackage meas_package) {
-  /**
-   * TODO: Complete this function! Use lidar data to update the belief 
-   * about the object's position. Modify the state vector, x_, and 
-   * covariance, P_.
-   * You can also calculate the lidar NIS, if desired.
-   */
+void UKF::UpdateLidar(MeasurementPackage meas_package)
+{
+    /**
+     * TODO: Complete this function! Use lidar data to update the belief
+     * about the object's position. Modify the state vector, x_, and
+     * covariance, P_.
+     * You can also calculate the lidar NIS, if desired.
+     */
 }
 
-void UKF::UpdateRadar(MeasurementPackage meas_package) {
-  /**
-   * TODO: Complete this function! Use radar data to update the belief 
-   * about the object's position. Modify the state vector, x_, and 
-   * covariance, P_.
-   * You can also calculate the radar NIS, if desired.
-   */
+void UKF::UpdateRadar(MeasurementPackage meas_package)
+{
+    /**
+     * TODO: Complete this function! Use radar data to update the belief
+     * about the object's position. Modify the state vector, x_, and
+     * covariance, P_.
+     * You can also calculate the radar NIS, if desired.
+     */
 }
 
 void UKF::InitUKF(const MeasurementPackage &meas_package)
 {
     // Initialize filter based on type of measurement
-    if(meas_package.sensor_type_ == MeasurementPackage::SensorType::LASER)
+    if (meas_package.sensor_type_ == MeasurementPackage::SensorType::LASER)
     {
         // Laser measurement
         double px = meas_package.raw_measurements_[0];
